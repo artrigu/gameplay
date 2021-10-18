fdb0dec8538539ce38a4417748d7be436a254aae
Arthur Passos <arthur.passos@outlook.com>
2021-10-18 16:02:57 -0300

    firstCommit

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+    View, 
+    Text,
+    Image,
+    StatusBar 
+ } from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+
+//Tela de login
+export function SignIn(){
+    return(
+        <View style={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+
+            />
+
+            <Image 
+                source={IllustrationImg} 
+                style={styles.image} 
+                resizeMode="stretch"
+            />
+        <View style={styles.content}>
+            <Text style={styles.title}>
+            Conecte-se{'\n'}
+            e organize suas{'\n'}
+            jogatinas
+            </Text> 
+
+            <Text style={styles.subtitle}>
+                Crie grupos para jogar seus games{'\n'}
+                favoritos com seus amigos
+            </Text>
+            <ButtonIcon 
+            title="Entrar com Discord"
+            activeOpacity={.8}
+            />
+
+        </View>
+
+        </View>
+    );
+}
